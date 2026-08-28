b0abc329b84cd9e04923d94f03d40de3af832443
LuckDuckTCS <antonov.semen2015@gmail.com>
2026-08-28 16:12:29 +0300

    feat: Счётчик слов из io.Reader

---
+++ internal/wordfreq/count.go
@@ -0,0 +1,49 @@
+package wordfreq
+
+import (
+	"bufio"
+	"cmp"
+	"io"
+	"slices"
+	"strings"
+)
+
+type Pair struct {
+	Word  string
+	Count int
+}
+
+func Count(r io.Reader) ([]Pair, error) {
+	result := []Pair{}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	m := make(map[string]int)
+	// читаем
+	for scanner.Scan() {
+		word := scanner.Text()
+		if scanner.Err() != nil {
+			word = strings.ToLower(word)
+			word = strings.Trim(word, ".,!?;:-()\"'")
+			m[word]++
+		}
+
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	// из карты в срез
+	for key, value := range m {
+		result = append(result, Pair{Word: key, Count: value})
+	}
+
+	// Сортируем
+	slices.SortFunc(result, func(a, b Pair) int {
+		if n := cmp.Compare(b.Count, a.Count); n != 0 {
+			return n
+		}
+		return cmp.Compare(a.Word, b.Word)
+	})
+
+	return result, nil
+}
